35ccefee2cc2af692f34671cf4e8556812c3c661
pw11-085 <yogi.krsh22@gmail.com>
2023-06-10 18:37:05 +0530

    Bot Added

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React from "react";
+import Hero from "./Components/Hero";
+// import About from "./Components/About";
+// import Skills from "./Components/Skills";
+// import Project from "./Components/Project";
+// import Hireme from "./Components/Hireme";
+// import Contact from "./Components/Contact";
+// import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Bot from "./Components/Bot";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
+    <div>
+      <Navbar />
+      <Bot />
+      {/* <Hero /> */}
+      {/*<About />
+      <Skills />
+      <Hireme />
+      <Project />
+      <Contact />
+      <Footer /> */}
     </div>
   );
-}
+};
 
 export default App;
